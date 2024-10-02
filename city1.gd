53963199679264527f1f0908cf31b5eaee6d686c
William Lopez <126110989+heady8354@users.noreply.github.com>
2024-10-02 09:10:01 -1000

    indev v0.0.2 || Functional PT income
    In this update I have gotten the meat and bones of the Public Transport income system working. There were issues with how it interacted with the skip dialogue function, but it was resolved easily. Next update, I will be focusing on the UI and PTincome QoL.

--- city1.gd
+++ city1.gd
@@ -39,8 +39,10 @@ var incomeRateCost = 1000
 var incomeRate = 30
 var happinessLowerRate = 45
 var income = 200
+var incomeRateLvl = 1
+var incomeLvl = 1
 var busCount = 0
-var PTincome = 0
+var PTincome = busCount * 100
 var parkCost = 10000
 var gotStore1 = false
 var gotStore2 = false
@@ -48,18 +50,37 @@ var parkFix = false
 var popup1 = null
 var popup2 = null
 var incomePopup = null
+var hqPopup = null
 var store1Lvl = 0
 var store2lvl = 0
 var upgradeStore1Available = false
 var upgradeStore2Available = false
 var skipped = false
+var bus1popup = null
+var bus2popup = null
+var bus3popup = null
+var bus4popup = null
+var bus5popup = null
+var bus1bought = false
+var bus2bought = false
+var bus3bought = false
+var busb4ought = false
+var bus5bought = false
+var COTpopup = null
 #civ texts
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	popup1 = $Store1.get_popup()
 	popup2 = $Store2.get_popup()
+	bus1popup = $Bus1.get_popup()
+	bus2popup = $Bus2.get_popup()
+	bus3popup = $Bus3.get_popup()
+	bus4popup = $Bus4.get_popup()
+	bus5popup = $Bus5.get_popup()
 	incomePopup = $Funds/Incomes.get_popup()
+	hqPopup = $HQ.get_popup()
+	COTpopup = $COT.get_popup()
 	label_node = $Camera2D/Commander/Textbox/Text
 	commander = $Camera2D/Commander
 	commander.texture = commanderhappy
@@ -142,18 +163,27 @@ func _ready():
 		$AnimationPlayer.play("hint")
 		popup1.connect("id_pressed", Callable(self, "_on_item_pressed"))
 		popup2.connect("id_pressed", Callable(self, "_on_item_pressed2"))
+		hqPopup.connect('id_pressed', Callable(self, "_on_item_pressedHQ"))
+		bus1popup.connect('id_pressed',Callable(self, "_on_item_pressedBus1"))
+		bus2popup.connect('id_pressed',Callable(self, "_on_item_pressedBus2"))
+		bus3popup.connect('id_pressed',Callable(self, "_on_item_pressedBus3"))
+		bus4popup.connect('id_pressed',Callable(self, "_on_item_pressedBus4"))
+		bus5popup.connect('id_pressed',Callable(self, "_on_item_pressedBus5"))
+		COTpopup.connect('id_pressed', Callable(self, "_on+item_pressedCOT"))
 		incomePopup.add_item("Total income: $" + str(income) +  "/" + str(incomeRate) + "s \n Income from public transport: $" + str(PTincome) + "/10s", 0)
 		var happinessTimer = $Timer
 		var incomeTimer = $Timer2
 		var PTincomeTimer = $PTincomeTimer
-		PTincomeTimer.start()
 		PTincomeTimer.wait_time = 10
+		PTincomeTimer.start()                                       #REMEMBER TO ADD IT TO THE SKIPPED FUNC AT THE BOTTOM!!!!!!!!!!!!!!!
 		happinessTimer.start()
 		incomeTimer.start()
 		incomeTimer.wait_time = incomeRate
 		happinessTimer.wait_time = happinessLowerRate
 		incomeTimer.connect("timeout",Callable(self, "_on_incometimer_timeout"))
 		happinessTimer.connect("timeout",Callable(self, "_on_happinesstimer_timeout"))
+		PTincomeTimer.connect('timeout',Callable(self, "_on_ptincometimer_timeout"))
+		$skipDialogue.visible = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if store1Lvl == 2:
@@ -161,15 +191,22 @@ func _process(delta):
 		popup1.remove_item(0)
 		popup1.add_item('Store is max lvl! (I appreciate the ambition though.)' , 9)
 		popup1.add_item('Close',8)
+	if store2lvl == 2:
+		popup2.clear()
+		popup2.add_item('Store is max lvl!', 9)
+		popup2.add_item('Close' , 8) 
 	$Happiness/Label.text = "Happiness: " + str(happiness) + '%'
 	$Funds/Label.text = "$" + str(fund)
 	if happiness < 26:
 		$Happiness.color = Color(1,0,0)
-	if happiness > 49 and not happiness < 26:
+	if happiness > 49 and happiness > 26:
 		$Happiness.color = Color(1,2,0)
 	if happiness > 74 and not happiness < 51 and not happiness < 26:
 		$Happiness.color = Color(0,1,0)
-
+	incomePopup.clear()
+	incomePopup.add_item("Income from stores: $" + str(income) +  "/" + str(incomeRate) + "s",0)
+	incomePopup.add_item("Income from public transport: $" + str(PTincome) + "/10s", 1)
+	PTincome = busCount * 100
 
 func _on_happinesstimer_timeout():
 		happiness = happiness - 3
@@ -185,6 +222,11 @@ func _on_incometimer_timeout():
 			print('store 2 not detected')
 		if gotStore2 == true:
 			$store2incomeAnim.play('store2IncomeMade')
+func _on_ptincometimer_timeout():
+	print('ptincome gained')
+	if busCount > 0:
+		$COTincomeAnim.play('COTincome')
+		fund = fund + PTincome
 func _on_item_pressed(id):
 	match id:
 		1:
@@ -195,7 +237,7 @@ func _on_item_pressed(id):
 				gotStore1 = true
 				income = income + 100
 				happiness = happiness + 10
-				$Store1IncomeTxt.text = "$100" 
+				$Store1IncomeTxt.text = "$$$" 
 				fund = fund - 2500
 				popup1.remove_item(0) #0 double (or whatever-ple) zero to remove everything
 				popup1.remove_item(0) 
@@ -215,7 +257,7 @@ func _on_item_pressed(id):
 				fund = fund - 1500
 				income = income + 100
 				happiness = happiness + 10
-				$Store1IncomeTxt.text = "$200"
+				$Store1IncomeTxt.text = "$$$"
 				$Store1Lvl1.visible = false
 				$Store1Lvl2.visible = true
 				$AnimationPlayer.stop()
@@ -225,15 +267,17 @@ func _on_item_pressed(id):
 				print('store 1 upgraded')
 			if upgradeStore1Available == true and store1Lvl == 1 and fund < 1500:
 				print('cannot afford lvl 2 for stroe 1')
-func _on_item_pressed2(id):
+func _on_item_pressed2(id):	
 	match id:
 		1:
 			if gotStore2 == false and fund > 2500:
 				$Hint.visible = false
 				gotStore2 = true
+				$Hint2.visible = true
+				$AnimationPlayer2.play('hint2')
 				income = income + 100
 				happiness = happiness + 10
-				$Store2IncomeTxt.text = "$100" 
+				$Store2IncomeTxt.text = "$$$" 
 				fund = fund - 2500
 				popup2.remove_item(0) #0 double (or whatever-ple) zero to remove everything
 				popup2.remove_item(0) 
@@ -253,7 +297,7 @@ func _on_item_pressed2(id):
 				fund = fund - 1500
 				income = income + 100
 				happiness = happiness + 10
-				$Store2IncomeTxt.text = "$200"
+				$Store2IncomeTxt.text = "$$$"
 				$Store2Lvl1.visible = false
 				$Store2Lvl2.visible = true
 				$AnimationPlayer.stop()
@@ -263,8 +307,72 @@ func _on_item_pressed2(id):
 				print('store 2 upgraded')
 			if upgradeStore2Available == true and store2lvl == 1 and fund < 1500:
 				print('cannot afford lvl 2 for stroe 2')
-
-
+func _on_item_pressedHQ(id):
+	match id:
+		2:
+			if incomeLvl == 4:
+				hqPopup.set_item_disabled(0, true)
+				hqPopup.set_item_text(0, "IncomeLvl is max! Arent you rich enough?")
+				print(hqPopup.get_item_text(0))
+			else:
+				incomeLvl = incomeLvl + 1
+				fund = fund - 3000
+				print('income upgraded by 1.5x!')
+				income = income * 1.5
+				$AnimationPlayer2.stop()
+				$AnimationPlayer2.seek(0, true)
+				$AnimationPlayer2.play('incomeUpg')
+		1: 
+			if incomeRateLvl == 6:
+				hqPopup.set_item_disabled(1, true)
+				hqPopup.set_item_text(1, 'IncomeRateLvl is max! ')
+			else:
+				incomeRateLvl = incomeRateLvl + 1
+				fund = fund - 1000
+				print('income timer upgraded! is now ' + str(incomeRate) + "s")
+				incomeRate = incomeRate - 5
+				$Timer2.wait_time = incomeRate
+				$AnimationPlayer2.stop()
+				$AnimationPlayer2.seek(0, true)
+				$AnimationPlayer2.play("incomeRateUpg")
+func _on_item_pressedBus1(id):
+	match id:
+		1:
+			if fund > 750 and not bus1bought:
+				happiness = happiness + 5
+				fund = fund - 750
+				$AnimationPlayer2.play('buildBus1')
+				await(get_tree().create_timer(0.5)).timeout
+				$Bus1Tiles.visible = true
+				bus1bought = true
+				bus1popup.set_item_text(0, "Bus stop purchased!")
+			if fund < 750 or bus1bought:
+				print('Failed to purchase bus1, either youre too BROKE or you already have it.')
+func _on_item_pressedBus2(id):
+	match id:
+		1:
+			if fund > 750 and not bus2bought:
+				happiness = happiness + 5
+				fund = fund - 750
+				$AnimationPlayer2.stop()
+				$AnimationPlayer2.seek(0, true)
+				$AnimationPlayer2.play('buildBus2')
+				await(get_tree().create_timer(0.5)).timeout
+				$Bus2Tiles.visible = true
+				bus2bought = true
+				bus2popup.set_item_text(0, "Bus stop purchased!")
+			if fund < 750 or bus2bought:
+				print('Failed to purchase bus2, either youre too BROKE or you already have it.')
+#add the rest of the bus popups
+func _on_item_pressedCOT(id):
+	match id:
+		1: 
+			if fund > 3000:
+				fund = fund - 3000
+				busCount = busCount + 1
+				$AnimationPlayer2.stop()
+				$AnimationPlayer2.seek(0, true)
+				$AnimationPlayer2.play('COTaddBus')
 
 
 #text funcs
@@ -350,18 +458,25 @@ func _on_skip_dialogue_pressed():
 	$AnimationPlayer.play("hint")
 	popup1.connect("id_pressed", Callable(self, "_on_item_pressed"))
 	popup2.connect("id_pressed", Callable(self, "_on_item_pressed2"))
-	incomePopup.add_item("Total income: $" + str(income) +  "/" + str(incomeRate) + "s \n Income from public transport: $" + str(PTincome) + "/10s", 0)
+	hqPopup.connect('id_pressed', Callable(self, "_on_item_pressedHQ"))
+	COTpopup.connect('id_pressed',Callable(self, "_on_item_pressedCOT"))
 	var happinessTimer = $Timer
 	var incomeTimer = $Timer2
 	var PTincomeTimer = $PTincomeTimer
-	PTincomeTimer.start()
 	PTincomeTimer.wait_time = 10
+	PTincomeTimer.start()
 	happinessTimer.start()
 	incomeTimer.start()
 	incomeTimer.wait_time = incomeRate
 	happinessTimer.wait_time = happinessLowerRate
 	incomeTimer.connect("timeout",Callable(self, "_on_incometimer_timeout"))
 	happinessTimer.connect("timeout",Callable(self, "_on_happinesstimer_timeout"))
+	PTincomeTimer.connect('timeout',Callable(self, "_on_ptincometimer_timeout"))
+	bus1popup.connect('id_pressed',Callable(self, "_on_item_pressedBus1"))
+	bus2popup.connect('id_pressed',Callable(self, "_on_item_pressedBus2"))
+	bus3popup.connect('id_pressed',Callable(self, "_on_item_pressedBus3"))
+	bus4popup.connect('id_pressed',Callable(self, "_on_item_pressedBus4"))
+	bus5popup.connect('id_pressed',Callable(self, "_on_item_pressedBus5"))
 
 
 
